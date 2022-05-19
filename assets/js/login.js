ed0b95a1ceadc66b0d419f22c9ac4e2e47406271
super.rui <1984485320@qq.com>
2022-05-19 09:41:38 +0800

    修改login.js 与login.css

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,12 @@
 $(function() {
+    // 点击去注册账号让 登录框隐藏，注册框显示
     $('#link_reg').click(function() {
         $('.login-box').hide();
         $('.reg-box').show();
-    })
+    });
+    // 点击去登录让 注册框隐藏，登录框显示
     $('#link_login').click(function() {
         $('.login-box').show();
         $('.reg-box').hide();
-    })
+    });
 })
