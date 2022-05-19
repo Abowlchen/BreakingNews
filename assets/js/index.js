e3aacd9c4c1079becc0d6117459ac9e4f9b5bb2a
super.rui <1984485320@qq.com>
2022-05-19 18:33:02 +0800

    丰富主页

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+$(function() {
+    getUserInfo();
+
+});
+const layer = layui.layer;
+
+function getUserInfo() {
+    $.ajax({
+        type: 'GET',
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem("token"),
+        },
+        success: (res) => {
+            if (res.status !== 0) {
+                return layer.msg('获取信息失败！');
+            }
+            layer.msg('获取信息成功！');
+            renderAvatar(res.data);
+        },
+    });
+};
+
+function renderAvatar(user) {
+    console.log(user);
+    const uname = user.nickname || user.username;
+    $('#welcome').html(`欢迎${uname}`);
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        $('.text-avatar').html(`${uname[0].toUpperCase()}`).show();
+    }
+}
